0fdccd94fd79d11ac8eec55409ba457af9067459
baIder <511763880@qq.com>
2022-08-11 16:58:32 +0800

    Footer样式

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  padding: 10px 100px;
+  text-align: center;
+  font-size: 12px;
+  color: #aaa;
+`;
+
 function Footer() {
   return (
     <>
-      <footer>Footer</footer>
+      <StyledFooter>Footer</StyledFooter>
     </>
   );
 }

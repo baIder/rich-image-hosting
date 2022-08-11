33c3a2c8e2b525835c9cae75deb2292f7a5aaaf9
baIder <511763880@qq.com>
2022-08-11 16:10:42 +0800

    Header结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import History from './pages/History';
+import About from './pages/About';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/history" element={<History/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
-function Header(){
-  return (
-    <>
-      <h1>Header</h1>
+import Logo from './logo.svg';
+import {Link} from 'react-router-dom';
 
-    </>
-  )
+function Header() {
+  return (
+    <div>
+      <img src={Logo} alt=""/>
+      <nav>
+        <Link to="/">首页</Link>
+        <Link to="/history">历史</Link>
+        <Link to="/about">关于</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;

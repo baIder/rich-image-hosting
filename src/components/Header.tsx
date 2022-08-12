667768e1f5ee99e531623bba70e17fed10462efb
baIder <511763880@qq.com>
2022-08-12 15:18:51 +0800

    引入antd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import LogoURL from './logo.svg';
 import {NavLink} from 'react-router-dom';
 import styled from 'styled-components';
+import {Button} from 'antd';
 
 const StyledHeader = styled.header`
   display: flex;
+
   align-items: center;
   padding: 10px 100px;
   background: #02101f;
@@ -21,6 +23,15 @@ const StyledLink = styled(NavLink)`
   }
 `;
 
+const Login = styled.div`
+  margin-left: auto;
+`;
+
+const StyledButton = styled(Button)`
+  margin-left: 10px;
+`;
+
+
 function Header() {
   return (
     <StyledHeader>
@@ -30,6 +41,10 @@ function Header() {
         <StyledLink to="/history">上传历史</StyledLink>
         <StyledLink to="/about">关于我</StyledLink>
       </nav>
+      <Login>
+        <StyledButton type="primary">登录</StyledButton>
+        <StyledButton type="primary">注册</StyledButton>
+      </Login>
     </StyledHeader>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,24 @@
-function Register() {
+import {useStores} from '../stores';
+import {observer} from 'mobx-react';
+import {useRef} from 'react';
+
+
+const Register = () => {
+  const {AuthStore} = useStores();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const bindChange = () => {
+    console.log(inputRef.current!.value);
+    AuthStore.setUsername(inputRef.current!.value);
+    console.log(AuthStore.values.username);
+  };
   return (
     <>
-      <h1>Register</h1>
-
+      <h1>Register:{AuthStore.values.username}</h1>
+      <input onChange={bindChange} ref={inputRef}/>
     </>
   );
-}
+};
 
-export default Register;
+export default observer(Register);

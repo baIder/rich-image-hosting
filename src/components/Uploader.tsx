eed76a9067a4fd8ab8ec03209043eb27178cd49c
baIder <511763880@qq.com>
2022-08-14 15:12:14 +0800

    实现上传成功后的效果

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,31 +1,74 @@
 import React from 'react';
 import {useStores} from '../stores';
-import {message, Upload} from 'antd';
+import {message, Upload, Input, Space} from 'antd';
 import type {UploadProps} from 'antd';
 import {InboxOutlined} from '@ant-design/icons';
-import {observer} from 'mobx-react';
+import {observer, useLocalObservable} from 'mobx-react';
+import styled from 'styled-components';
 
 const {Dragger} = Upload;
 
+const Result = styled.div`
+  margin-top: 10px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`;
+
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`;
+
+const Image = styled.img`
+  max-width: 300px;
+`;
+
 const Uploader: React.FC = () => {
-  const {ImageStore} = useStores();
+  const {ImageStore, UserStore} = useStores();
+  const store = useLocalObservable(() => ({
+    width: '',
+    height: '',
+    setWidth(width: string) {
+      store.width = width;
+    },
+    setHeight(height: string) {
+      store.height = height;
+    },
+    get widthStr() {
+      return store.width === '' ? '' : `/w/${store.width}`;
+    },
+    get heightStr() {
+      return store.height === '' ? '' : `/h/${store.height}`;
+    },
+    get fullStr() {
+      return ImageStore?.serverFile?.attributes.url.attributes.url + '?imageView2/0' + store.widthStr + store.heightStr;
+    }
+  }));
+  const changeWidth = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    store.setWidth(e.target.value);
+  };
+  const changeHeight = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    store.setHeight(e.target.value);
+  };
   const props: UploadProps = {
     name: 'file',
     showUploadList: false,
     beforeUpload(file) {
+      if (UserStore.currentUser === '') {
+        message.warning('请先登录再上传！').then();
+        return false;
+      }
       ImageStore.setFile(file);
       ImageStore.setFilename(file.name);
       ImageStore.upload()
-        .then(serverFile => {
+        .then((serverFile) => {
           message.success('上传成功').then();
           console.log(serverFile);
         })
-        .catch(() => message.error('上传失败').then())
-      ;
+        .catch(() => message.error('上传失败').then());
       return false;
-    }
+    },
   };
-
 
   return (
     <>
@@ -33,18 +76,41 @@ const Uploader: React.FC = () => {
         <p className="ant-upload-drag-icon">
           <InboxOutlined/>
         </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
         <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-          band files
+          Support for a single or bulk upload. Strictly prohibit from uploading
+          company data or other band files
         </p>
       </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {
-          ImageStore.serverFile ? <div>{ImageStore.serverFile.attributes.url.attributes.url}</div> : null
-        }
-      </div>
+      {
+        ImageStore.serverFile ? <Result>
+          <H1>上传结果</H1>
+          <dl>
+            <dt>图片地址：</dt>
+            <dd><a target="_blank"
+                   href={ImageStore.serverFile.attributes.url.attributes.url}
+                   rel="noreferrer">{ImageStore.serverFile.attributes.url.attributes.url}</a>
+            </dd>
+            <dt>文件名：</dt>
+            <dd>{ImageStore.filename}</dd>
+            <dt>图片预览：</dt>
+            <dd>
+              <Image src={ImageStore.serverFile.attributes.url.attributes.url} alt=""/>
+            </dd>
+            <dt>自定义尺寸：</dt>
+            <dd>
+              <Space direction="horizontal">
+                <Input addonBefore="宽度" addonAfter="px" placeholder="输入自定义的宽度（可选）" allowClear
+                       onChange={changeWidth}/>
+                <Input addonBefore="高度" addonAfter="px" placeholder="输入自定义的高度（可选）" allowClear onChange={changeHeight}/>
+              </Space>
+            </dd>
+            <dd>{store.fullStr}</dd>
+          </dl>
+        </Result> : null
+      }
     </>
   );
 };

b546969a866554dad75bc68034ab329d39411f5b
baIder <511763880@qq.com>
2022-08-15 19:21:36 +0800

    修改Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Spin} from 'antd';
+import styled from 'styled-components';
+
+const IsLoading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Loading: React.FC = () => {
   return (
-    <div>Loading..</div>
+    <IsLoading><Spin size="large"/></IsLoading>
   );
 };
 

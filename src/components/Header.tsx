f6ef5054d7f5e330a0abb5793b92bbb9cc4a2124
baIder <511763880@qq.com>
2022-08-15 18:31:27 +0800

    调整样式

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import {observer} from 'mobx-react';
 import React from 'react';
 
 const StyledHeader = styled.header`
+  height: 52px;
   display: flex;
   align-items: center;
   padding: 10px;
@@ -19,15 +20,57 @@ const Logo = styled.img`
     display: none;
   }
 `;
+const Nav = styled.nav`
+  height: 52px;
+  display: flex;
+  align-items: end;
+`;
 const StyledLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 52px;
   color: #fff;
-  margin-left: 30px;
-  @media (max-width: 500px) {
-    margin-left: 10px;
+  margin-left: 10px;
+  @media (min-width: 500px) {
+    margin-left: 30px;
+    width: 100px;
+    height: 42px;
+    position: relative;
+    background: rgba(240, 242, 245, 0.5);
+    color: #ddd;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
   }
+}
 
-  &.active {
-    border-bottom: 1px solid #fff;
+&.active {
+  border-bottom: 1px solid #fff;
+  @media (min-width: 500px) {
+    border-bottom: none;
+    background: rgba(240, 242, 245, 1);
+    color: #333;
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: -10px;
+      width: 10px;
+      height: 10px;
+      border-bottom-right-radius: 10px;
+      box-shadow: 5px 5px 2px 2px #F0F2F5;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      right: -10px;
+      width: 10px;
+      height: 10px;
+      border-bottom-left-radius: 10px;
+      box-shadow: -5px 5px 2px 2px #F0F2F5;
+    }
   }
 `;
 const Login = styled.div`
@@ -52,10 +95,14 @@ const Header: React.FC = () => {
   return (
     <StyledHeader>
       <Logo src={LogoURL} alt=""/>
-      <nav>
-        <StyledLink to="/">首页</StyledLink>
-        <StyledLink to="/history">上传历史</StyledLink>
-      </nav>
+      <Nav>
+        <StyledLink to="/">
+          <div>首页</div>
+        </StyledLink>
+        <StyledLink to="/history">
+          <div>上传历史</div>
+        </StyledLink>
+      </Nav>
       <Login>
         {
           UserStore.currentUser ? <>
